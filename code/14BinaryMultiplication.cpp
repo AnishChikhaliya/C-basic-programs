f60c21ecd1345ea492ec829b03b0cead20fa43fa
Anish Chikhaliya <anishchikhaliya02@gmail.com>
2022-06-02 17:19:14 -1100

    Add Sieve algorithem

--- code/14BinaryMultiplication.cpp
+++ code/14BinaryMultiplication.cpp
@@ -21,3 +21,26 @@ int main(){
     cout << binarymul(102241, 102485);
 }
 
+// Print N=1e7+10 size to prime number 
+// Sieve Algorithem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e7+10;
+vector<bool> isPrime(N, 1);
+
+int main (){
+    
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i < N; ++i){
+        if(isPrime[i] == true){
+            for(int j = 2*i; j < N; j+=i){
+                isPrime[j] = false;
+            }
+        }
+    }
+    for(int i=1; i < 100; ++i){
+        cout << isPrime[i] << endl;
+    }
+}
